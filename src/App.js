6020f7592500a707b721977582ac8e509c9778cc
Ícaro Novo de Oliveira <icaronovo@outlook.com>
2023-03-10 00:20:40 -0800

    Created the routing system, as well as the initial css styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
+import NavigationBar from "./components/navigation_bar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home.js"
+import Newsletter from "./pages/newsletter.js"
+import Results from "./pages/results.js"
+import Teams from "./pages/teams.js"
+import Login from "./pages/login.js"
+import SignUp from "./pages/signup.js"
 
 function App() {
   return (
-   null
+    <>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/newsletter" element={<Newsletter />}></Route>
+        <Route path="/results" element={<Results />}></Route>
+        <Route path="/teams" element={<Teams />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+      </Routes>
+    </>
   );
 }
 
