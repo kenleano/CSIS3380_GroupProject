a847261027fc92111b8998fc532c5ee76f2162ab
kenleano <kenleano@gmail.com>
2023-04-11 13:54:00 -0700

    Aesthetic changes

--- src/components/geo.js
+++ src/components/geo.js
@@ -6,7 +6,7 @@ export default function Geo() {
         <div className="geo">
             <h3>Find the team closest to you!</h3>
             <div className="teamContainer">
-                <img src="../assets/logos/1.png" alt=""></img>
+                <img src="../assets/logos/3.png" alt=""></img>
                 <h2>Surrey Storm</h2>
                 <h3>Surrey Avenue, Surrey/BC</h3>
                 <h3>Accepting admissions!</h3>
