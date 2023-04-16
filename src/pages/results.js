fe3289f977795e8bc69c7eca95b175b956c592da
kenleano <kenleano@gmail.com>
2023-04-15 19:20:59 -0700

    Retrieve games from database

--- src/pages/results.js
+++ src/pages/results.js
@@ -1,9 +1,8 @@
 import MatchCard from "../components/matchCard"
+import "../css/matchCard.css";
 
 export default function Results() {
     return (
-        <div className="container" style={{  height: '100vh', overflowY: 'auto' }}>
-            <MatchCard />
-        </div>
+ <MatchCard />
     )
 }
