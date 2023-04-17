556874bb2004e29c79f346a5b749f70ec334f16f
Ícaro Novo de Oliveira <62024838+icaronovo@users.noreply.github.com>
2023-04-16 21:50:39 -0700

    added a small log

--- src/components/geo.js
+++ src/components/geo.js
@@ -35,6 +35,7 @@ export default function Geo() {
         var dMin = 99; //distance placeholder. The biggest number that could be compared two is 3.14
 
         teams.forEach(team => {
+            console.log("Geotag lenght: " + team.geotag.lenght)
             if (team.geotag.lenght > 0) {
                 var lat1 = parseFloat(team.geotag[0].$numberDecimal); //get team latitude
                 var lon1 = parseFloat(team.geotag[1].$numberDecimal); //get team longitude
